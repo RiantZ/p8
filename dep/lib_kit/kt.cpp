220eeafa9c6bd3d61c10adc6372e7ecb75d3a4ba
RiantZ <tekodont.z@gmail.com>
2025-12-22 21:36:59 +0100

    add skeleton of lib_kit

---
+++ dep/lib_kit/kt.cpp
@@ -0,0 +1,6 @@
+#pragma once
+
+//emtpy file, all library files will be included directly.
+//the file exists to let CMake to create library and show it in IDE as another folder
+                      
+#include "list.hpp"
